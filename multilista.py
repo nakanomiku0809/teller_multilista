8b4d343f2c95d967b0177f0f335b687e0bdaade6
mikunino <moisesdj@uninorte.edu.co>
2026-05-13 16:35:28 +0000

    ajustes

--- multilista.py
+++ multilista.py
@@ -1,35 +1,63 @@
+import csv
+import files
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.head=None
-        self.tail=None
-        self.sublista=None
-class LinkedList:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.next = None
+        self.prev = None
+        self.sub_list = None
+
+class DobleLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
     
-    def leer():
-        lista = []
-        with open("DIVIPOLA-_C_digos_municipios_20250505.csv", "r", encoding="utf-8") as archivo:
-            for linea in archivo:
-                datos = linea.strip().split(",")
-                Node = Node(
-                    int(datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],datos[6])
-                )
-                lista.append(Node)
-        return lista
-    def añadir(self,Node,data):
-        data=lista
-        for lista in 1:
-            if Node.sublista==None:
-                sublista=LinkedList()
-                sublista.head=data
-                sublista.tail=data
-                Node.sublista=sublista
-            else:
-                current=Node.sublista.tail
-                current.next=data
-                data.prev=current
-                Node.subllista.tail=data
-        return Node.sublista
+    def leer(self, file_path):
+        countries = DoubleLinkedList()
+        colombia = Country("CO", "Colombia")
+        countries.append(colombia)
+
+        departments = {}
+
+        with open(file_path, encoding="utf-8-sig") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                dept_code = row["cod_depto"]
+                dept_name = row["departamento"]
+
+                city_code = row["cod_mpio"]
+                city_name = row["municipio"]
+
+                lat = row["lat"]
+                lon = row["lon"]
+
+    def add_child(self, parent, child):
+        if parent.sub_list is None:
+            sublist = DoubleLinkedList()
+            sublist.head = child
+            sublist.tail = child
+            parent.sub_list = sublist
+        else:
+            current = parent.sub_list.tail
+            current.next = child
+            child.prev = current
+            parent.sub_list.tail = child
+
+        return parent.sub_list
+    
+    def mostrar(self,level=0):
+        if self.head is None:
+            print("lista vacia")
+            return
+        while current:
+            print(" "*level+str(current)
+            if current.sublista:
+                current.sublista.mostrar(level +1)
+            current=current.next
+    
+    def buscar(self,attr,value):
+        current=self.head
+
+        
